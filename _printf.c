13cdfb76c543ddd6801badc63b2de636d1f30a1a
Nada hamdani <nada.hamdani.nh@gmail.com>
2023-11-11 12:42:45 -0800

    Edit code printf

--- _printf.c
+++ _printf.c
@@ -1,51 +1,65 @@
-#include "main.h"
+`#include "main.h"
+#include <unistd.h>
 
-/**
- * _printf - a custom printf function
- * @format: string with or without format specifiers
- *
- * Return: returns the number of characters printed
- */
 int _printf(const char *format, ...)
 {
-	int count, c;
-	char *str;
-	va_list args;
+    char buffer[1024];
+    int count = 0;
+    int buf_index = 0;
 
-	if (format == NULL)
-		return (-1);
-	count = 0;
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			count += write(1, format, 1);
-		}
-		else
-		{
-			format++;
-			if (*format != 'c' && *format != 's' && *format != '%')
-				return (-1);
-			switch (*format)
-			{
-				case 'c':
-					c = va_arg(args, int);
-					count += write(1, &c, 1);
-					break;
-				case 's':
-					str = va_arg(args, char*);
-					if (str == NULL)
-						count += write(1, "(null)", 6);
-					else
-						count += write(1, str, (strlen(str)));
-					break;
-				case '%':
-					count += write(1, format, 1);
-					break;
-			}
-		}
-		format++;
-	}
-	return (count);
+    va_list args;
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            buffer[buf_index++] = *format;
+
+            if (buf_index == sizeof(buffer) - 1)
+            {
+                count += write(1, buffer, buf_index);
+                buf_index = 0;
+            }
+        }
+        else
+        {
+            format++;
+
+            if (*format != 'c' && *format != 's' && *format != '%')
+            {
+                return -1;
+            }
+
+            switch (*format)
+            {
+                case 'c':
+                    buffer[buf_index++] = (char)va_arg(args, int);
+                    break;
+                case 's':
+                    {
+                        char *str = va_arg(args, char*);
+                        if (str == NULL)
+                            buf_index += snprintf(buffer + buf_index, sizeof(buffer) - buf_index, "(null)");
+                        else
+                            buf_index += snprintf(buffer + buf_index, sizeof(buffer) - buf_index, "%s", str);
+                    }
+                    break;
+                case '%':
+                    buffer[buf_index++] = '%';
+                    break;
+            }
+        }
+
+        format++;
+    }
+
+    if (buf_index > 0)
+    {
+        count += write(1, buffer, buf_index);
+    }
+
+    va_end(args);
+    return count;
 }
+`
